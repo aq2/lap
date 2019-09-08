d133a0cf3b7daf58c34f7d8614bce0c979228783
angelo <angelo@fluxton.co.uk>
2019-09-08 13:17:26 +0100

    now wiv buttonz

--- src/php/show_studios_view.php
+++ src/php/show_studios_view.php
@@ -7,26 +7,42 @@
   $rows = $table['rows'];
 ?>
 
-<table class='show'>
-  <tr>
-    <?php
-      foreach ($cols as $col) {
-        echo "<th class='{$col}'>{$col}</th>";
-      }
-    ?>
-  </tr>
-  <?php
-    foreach ($rows as $row) {
-      $i = 0;
-      echo "<tr>";
-      foreach ($row as $datum) {
-        echo "<td class='{$cols[$i]}'>{$datum}</td>";
-        $i++;
-      }
-      echo "</tr>";
-    }
-  ?>
-</table>
-
+<form action='studios_controller.php'>
+  <table class='show'>
+    <col style='width:6%'>
+    <col style='width:25%'>
+    <col style='width:30%'>
+    <col style='width:15%'>
+    <col style='width:21%'>
+    <thead>
+      <tr>
+        <?php
+          foreach ($cols as $col) {
+            echo "<th class='{$col}'>{$col}</th>";
+          }
+        ?>
+        <th>action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        foreach ($rows as $row) {
+          $i = 0;
+          $j = 1;   # refers to row number - ie id
+          echo "<tr>";
+          foreach ($row as $datum) {
+            echo "<td class='{$cols[$i]}'>{$datum}</td>";
+            $i++;
+          }
+          echo "<td class='but'>";
+          echo "<input type='submit' value='delete' name='delete{$j}'>";
+          echo "<input type='submit' value='moar' name='moar{$j}'>";
+          echo "</td></tr>";
+          $j++;
+        }
+      ?>
+    </tbody>
+  </table>
+</form>
 
 
