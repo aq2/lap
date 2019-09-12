44b5ccf1d6ddd573ee681e8d18c1aa94e76fb21d
angelo <angelo@fluxton.co.uk>
2019-09-12 04:37:03 +0100

    selex in last table row OK

--- src/php/table_view.php
+++ src/php/table_view.php
@@ -1,54 +1,89 @@
 <?php
-main();
 
-function main() {
-  require_once('db_functions.php');
+require_once('db_functions.php');
 
-  $tableName = $_GET['tableName'];
+$tableName = $_GET['tableName'];
 
-  switch ($tableName) {
-    case 'studios':
-      $sql = "SELECT * FROM studios";
-      $col_widths = [6, 25, 30, 15, 21];
-      break;
-    case 'workshops':
-      $sql = "SELECT w.ws_id, w.date, w.time, w.type, s.name
-              FROM workshops w, studios s
-              WHERE w.studio_id = s.st_id";
-      $col_widths = [4, 2, 1];
+switch ($tableName) {
+  case 'studios':
+    $sql = "SELECT * FROM studios";
+    $col_widths = [6, 25, 30, 15, 21];
+    break;
+  case 'workshops':
+    $sql = "SELECT w.ws_id, w.date, w.time, w.type, s.name
+            FROM workshops w, studios s
+            WHERE w.studio_id = s.st_id";
+    $col_widths = [4, 2, 1];
 
-      $results = squery('SELECT DISTINCT type FROM workshops');
-      $types = array();
-      foreach ($results as $type) {
-        $types[] = $type['type'];
-      }
+    // $results = squery('SELECT DISTINCT type FROM workshops');
+    // $types = array();
+    // foreach ($results as $type) {
+    //   $types[] = $type['type'];
+    // }
 
-       $types = getOptions('SELECT DISTINCT type FROM workshops', 'type');
-      // $studios = getOptions('SELECT name FROM studios', 'name');
+    //   $types = getOptions('SELECT DISTINCT type FROM workshops', 'type');
+    // $studios = getOptions('SELECT name FROM studios', 'name');
 
-      $coltypes = [4 => $types, 5 => $studios];
-      break;
-  }
-
-
-
-  $table = getTable($tableName, $sql);
-  $cols = $table['cols'];
-  $rows = $table['rows'];
+    $specialColumns = [3 => 'workshops', 4 => 'studios'];
+    $specialNumbers = array_keys($specialColumns);
+    recho('specNum', $specialNumbers);
+    break;
 }
 
-  // merge this fn with makeSelectString
-  function getOptions($sql, $field) {
-    $options = array();
+
+$table = getTable($tableName, $sql);
+// recho('table', $table);
+$cols = $table['cols'];
+$rows = $table['rows'];
+
+$rows = injectSelect($rows, $cols, $specialColumns)[0];
+$selects = injectSelect($rows, $cols, $specialColumns)[1];
+
+// recho('new_rows', $new_rows);
+
+// foreach special, build select string, then inject into rows[x]
+function injectSelect($rows, $cols, $specialColumns) {
+  $selects = [];
+  foreach ($specialColumns as $col_num => $table) {
+    $fieldName = $cols[$col_num];
+    $sql = "SELECT DISTINCT $fieldName FROM {$table}";
     $results = squery($sql);
-    foreach ($results as $option) {
-      // recho('opt', $option);
-      // gecho('g', $option[$field]);
-      $options[] = $option[$field];   # don't work - syntax error unexpected {
+    // recho('res', $results);
+    $options = array();
+    foreach ($results as $k => $option) {
+      $options[] = $option[$fieldName];
     }
-    recho('opt', $options);
-    return $options;
+    // recho('opts', $options);
+    $select_html = makeSelectString($options, $fieldName);
+    $selects[] = $select_html;
+    recho('html', $select_html);
+    // $new_rows = array(array());
+    foreach ($rows as $ky => $row) {
+      $rows[$ky][$col_num] = $select_html;
+      // recho('row', $row);
+      // recho('rows', $rows);
+    }
   }
+
+      recho('selects', $selects);
+     // recho('rows', $rows);
+  return [$rows, $selects];
+}
+
+
+
+  // // merge this fn with makeSelectString
+  // function getOptions($sql, $field) {
+  //   $options = array();
+  //   $results = squery($sql);
+  //   foreach ($results as $option) {
+  //     // recho('opt', $option);
+  //     // gecho('g', $option[$field]);
+  //     $options[] = $option[$field];   # don't work - syntax error unexpected {
+  //   }
+  //   recho('opt', $options);
+  //   return $options;
+  // }
 
   function makeSelectString($options, $field) {
     // $options = ['aym', 'yoga', 'int']
@@ -102,15 +137,18 @@ function main() {
         echo "<tr class='input'>";
         echo "<th>{$j}</th>";
 
-        // TODO a bit hacky?
-        $k = 0;   # don't want first id column
-        foreach ($cols as $col) {
-          if ($k > 0) {
+        $num_cols = count($cols);
+        for ($c=1; $c<$num_cols; $c++) {
+          if (in_array($c, $specialNumbers)) {
+            $html = array_shift($selects);
+            echo "<th>{$html}</th>";
+          } else {
             echo "<th><input type='text' name={$col} required placeholder='add new {$tableName}'></th>";
           }
-          $k++;
         }
+
       ?>
+
       <th>
         <!-- TODO need clickhandlers -->
         <button id='cancel'>cancel</button>
